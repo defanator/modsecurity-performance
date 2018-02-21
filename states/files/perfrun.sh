58429f5eb04daa79de54f6a0f949cf5d55bfaba7
Andrei Belov <defanator@gmail.com>
2018-02-21 12:09:10 +0300

    perfrun: align request URI and headers with test/benchmark/benchmark.cc

--- states/files/perfrun.sh
+++ states/files/perfrun.sh
@@ -21,7 +21,19 @@ function run {
             echo "[${i}/${iterations}] $location ..."
             sudo service nginx start
             sleep 1
-            wrk -t${threads} -c${connections} -d${duration} -s ${HOME}/report.lua http://localhost${location}/a/b/c 2>&1 >>/tmp${location}.out
+            taskset -c 0-7 wrk -t${threads} -c${connections} -d${duration} -s ${HOME}/report.lua \
+                -H "Host: www.example.com" \
+                -H "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)" \
+                -H "Accept: text/html,application/xhtml+xml,application/xml; q=0.9,*/*;q=0.8" \
+                -H "Accept-Language: en-us,en;q=0.5" \
+                -H "Accept-Encoding: gzip,deflate" \
+                -H "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7" \
+                -H "Keep-Alive: 300" \
+                -H "Connection: keep-alive" \
+                -H "Cookie: PHPSESSID=r2t5uvjq435r4q7ib3vtdjq120" \
+                -H "Pragma: no-cache" \
+                -H "Cache-Control: no-cache" \
+                "http://localhost${location}/test.pl?param1=test&para2=test2" 2>&1 >>/tmp${location}.out
             sudo service nginx stop
             sleep 1
             i=$((i+1))
